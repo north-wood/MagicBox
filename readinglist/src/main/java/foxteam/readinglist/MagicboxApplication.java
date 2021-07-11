434ba46541a9d0af3f3a93d4010d1a26c9070740
Jeff Fox <luck7@live.cn>
2021-07-11 11:40:55 +0800

    module readinglist

---
+++ readinglist/src/main/java/foxteam/readinglist/MagicboxApplication.java
@@ -0,0 +1,20 @@
+package foxteam.readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class MagicboxApplication implements WebMvcConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicboxApplication.class, args);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "/readingList");
+	}
+
+}
